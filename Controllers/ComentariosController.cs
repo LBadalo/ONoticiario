de5ac26333d559abb1ee1a1409cea5f02b050f63
Luis Miguel Barbado Badalo <aluno19991@ipt.pt>
2020-05-06 18:45:38 +0100

    Criação dos Controllers

---
+++ Controllers/ComentariosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ONoticiario.Models;
+
+namespace ONoticiario.Controllers
+{
+    public class ComentariosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Comentarios
+        public ActionResult Index()
+        {
+            var comentarios = db.Comentarios.Include(c => c.Noticia).Include(c => c.Utilizador);
+            return View(comentarios.ToList());
+        }
+
+        // GET: Comentarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.NoticiasFK = new SelectList(db.Noticias, "ID", "Titulo");
+            ViewBag.UtilizadorFK = new SelectList(db.Utilizadores, "ID", "Username");
+            return View();
+        }
+
+        // POST: Comentarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Descricao,UtilizadorFK,Data,NoticiasFK")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Comentarios.Add(comentarios);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.NoticiasFK = new SelectList(db.Noticias, "ID", "Titulo", comentarios.NoticiasFK);
+            ViewBag.UtilizadorFK = new SelectList(db.Utilizadores, "ID", "Username", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.NoticiasFK = new SelectList(db.Noticias, "ID", "Titulo", comentarios.NoticiasFK);
+            ViewBag.UtilizadorFK = new SelectList(db.Utilizadores, "ID", "Username", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Descricao,UtilizadorFK,Data,NoticiasFK")] Comentarios comentarios)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comentarios).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.NoticiasFK = new SelectList(db.Noticias, "ID", "Titulo", comentarios.NoticiasFK);
+            ViewBag.UtilizadorFK = new SelectList(db.Utilizadores, "ID", "Username", comentarios.UtilizadorFK);
+            return View(comentarios);
+        }
+
+        // GET: Comentarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comentarios comentarios = db.Comentarios.Find(id);
+            if (comentarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comentarios);
+        }
+
+        // POST: Comentarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comentarios comentarios = db.Comentarios.Find(id);
+            db.Comentarios.Remove(comentarios);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
