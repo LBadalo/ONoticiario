46474233b383648470ca8833d056a3ef26cff993
LBadalo <aluno19991@ipt.pt>
2020-06-02 01:16:14 +0100

    Atualização da connectionstring

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -22,7 +22,7 @@ namespace ONoticiario.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("NoticiarioConString", throwIfV1Schema: false)
         {
         }
 
